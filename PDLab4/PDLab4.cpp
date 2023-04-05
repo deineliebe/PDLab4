0b77e801263d58c31bb1e4a4dc274cd3927dc5dd
nej-ive <nej-ve@users.noreply.github.com>
2023-04-05 06:57:31 +0300

    final version

--- PDLab4/PDLab4.cpp
+++ PDLab4/PDLab4.cpp
@@ -17,14 +17,13 @@ float estimate(float (*matrix), float (*x), float (*y), int n, int m)
     {
         yPred = 0;
         tempX = 1;
-        for (int j = m; j >= 0; j--)
+        for (int j = 0; j < m + 1; j++)
         {
             yPred += tempX * matrix[j];
             tempX *= x[i];
         }
         sum += (y[i] - yPred) * (y[i] - yPred);
     }
-    std::cout << '\n' << sum << '\n';
     return sum;
 }
 
@@ -44,17 +43,6 @@ void findCoefficients(float(*sums), float(*matrix), int m)
         equations[i][m + 1] = sums[i + 2 * (m + 1) - 1];
     }
 
-    //for (int i = 0; i < m + 1; i++)
-    //{
-    //    std::cout << '\n';
-    //    for (int j = 0; j < m + 2; j++)
-    //    {
-    //        std::cout << equations[i][j] << ' ';
-    //    }
-    //}
-    //std::cout << '\n';
-    //std::cout << '\n';
-
     float temp;
     for (int i = 0; i < m + 1; i++)
     {
@@ -71,19 +59,7 @@ void findCoefficients(float(*sums), float(*matrix), int m)
                 equations[j][k] -= equations[i][k] * temp;
             }
         }
-    }/*
-    std::cout << '\n';
-    std::cout << '\n';
-    for (int i = 0; i < m + 1; i++)
-    {
-        std::cout << '\n';
-        for (int j = 0; j < m + 2; j++)
-        {
-            std::cout << equations[i][j] << ' ';
-        }
     }
-    std::cout << '\n';
-    std::cout << '\n';*/
     for (int i = m; i >= 0; i--)
     {
         temp = equations[i][m + 1];
@@ -93,10 +69,6 @@ void findCoefficients(float(*sums), float(*matrix), int m)
         }
         matrix[i] = rnd(temp);
     }
-    /*for (int k = 0; k < m + 1; k++)
-    {
-        std::cout << matrix[k] << ' ';
-    }*/
 }
 
 void fillMatrixString(float(*arr), int n, int m, int start, float(*x), float(*y))
@@ -141,21 +113,7 @@ void fillMatrixString(float(*arr), int n, int m, int start, float(*x), float(*y)
         }
         matrix[n][j] = matrix[n][j];
     }
-
-    //for (int i = 0; i < n + 1; i++)
-    //{
-    //    std::cout << '\n';
-    //    for (int j = 0; j < 3 * (m + 1) - 1; j++)
-    //    {
-    //        std::cout << matrix[i][j] << ' ';
-    //    }
-    //}
-    //std::cout << '\n';
-    //std::cout << '\n';
-
-
     findCoefficients(matrix[n], arr, m);
-
 }
 
 void task1(float(*x), float(*y), int n, int m)
@@ -181,7 +139,7 @@ void task1(float(*x), float(*y), int n, int m)
     {
         matrix[i] = new float[m + 1];
         fillMatrixString(matrix[i], n, i, i, x, y);
-        tempEstimate = estimate(matrix[i], x, y, n, m);
+        tempEstimate = estimate(matrix[i], x, y, n, i);
         if (tempEstimate < bestEstimate)
         {
             bestEstimate = tempEstimate;
@@ -193,21 +151,20 @@ void task1(float(*x), float(*y), int n, int m)
     {
         std::cout << matrix[bestFunctionsIndex][i] << "x^" << bestFunctionsIndex - i << " + ";
     }
-    std::cout << matrix[bestFunctionsIndex][bestFunctionsIndex] << "\nIt's estimate: " << bestEstimate << '\n';
+    std::cout << matrix[bestFunctionsIndex][bestFunctionsIndex] << "\nIt's estimate: " << rnd(bestEstimate) << '\n';
 }
 
 int main()
 {
     std::cout << "This is the forth lab of project design.\n\n";
-    std::cout << "First task is to .\n";
-
-    std::cout << "Example:\n";
+    std::cout << "First task is to least squares approximation of a function.\n";
+    /*std::cout << "Example:\n";
     float x[5] = { 0.78, 1.56, 2.34, 3.12, 3.81 };
     float y[5] = { 2.50, 1.20, 1.12, 2.25, 4.28 };
-    task1(x, y, 5, 2);
+    task1(x, y, 5, 2);*/
 
-    /*float x[20];
+    float x[20];
     for (int i = 0; i < 20; i++) x[i] = 0.1 * i;
     float y[20] = { 3.15, 3.04, 3.02, 2.97, 2.87, 2.98, 2.81, 2.70, 2.66, 2.50, 2.60, 2.36, 2.09, 2.07, 2.01, 1.81, 1.53, 1.64, 1.29, 1.11 };
-    task1(x, y, 20, 4);*/
+    task1(x, y, 20, 4);
 }
